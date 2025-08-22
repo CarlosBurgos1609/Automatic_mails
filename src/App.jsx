bee3950fd2e176f0631c05e6f6e06528a58eb12a
Carlos Burgos <carlitosaburgosj1690@gmail.com>
2025-08-22 09:50:57 -0500

    Create Repositorio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import consejo from'./assets/images/consejo.png'
+
+function App() {
+  return (
+    <header>
+      Consejo superior de la judicatura
+      <img src={consejo} alt="logo del consejo" />
+    </header>
+  );
+}
+
+export default App;
