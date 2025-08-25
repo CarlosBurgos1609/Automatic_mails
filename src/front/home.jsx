38760cc391e5b90088668e8310496b7a4a9703d2
Carlos Burgos <carlitosaburgosj1690@gmail.com>
2025-08-25 09:43:16 -0500

    update app and create styles.scss

---
+++ src/front/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './../styles/styles.scss'
+
+
+const Home = () => {
+  return (
+    <div>
+        <h1>para</h1>
+      <p>hola</p>
+    </div>
+  )
+}
+
+export default Home
