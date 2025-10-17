da4ea26cd5ffe7a63c871906016a65f7bc57ca04
Carlos Burgos <carlitosaburgosj1609@gmail.com>
2025-10-17 10:01:35 -0500

    Update

--- src/conection/excelOneDrive.js
+++
@@ -1,7 +0,0 @@
-export async function fetchExcelFromOneDrive(url) {
-  // url: debe ser el enlace directo de descarga del archivo Excel
-  const response = await fetch(url);
-  if (!response.ok) throw new Error("No se pudo descargar el archivo Excel");
-  const blob = await response.blob();
-  return blob;
-}
